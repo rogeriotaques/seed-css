403dd234ba58f09e47d77dfab139fee2d2e99087
Rogerio Taques <rogerio.taques@rakuten.com>
2016-04-13 20:06:41 +0900

    Enhancements on index sample page and small bug fixes on grid.

--- source/js/main.js
+++ source/js/main.js
@@ -2,27 +2,38 @@
 
   var navbar, iniPos;
 
-  $(document).on('ready', function () {
-    navbar = $('section.page-navbar');
-    iniPos = navbar.offset().top;
+  $(document)
 
-    // on click handler for smooth scrolling
-    navbar
-      .find('a.spy')
-        .on('click', function (ev) {
-          ev.preventDefault();
-          $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top - iniPos});
-        });
+    .on('ready', function () {
+      navbar = $('section.page-navbar');
+      iniPos = navbar.offset().top;
 
-  }).on('scroll', function () {
+      // handler for smooth scrolling
+      navbar
+        .find('a.spy')
+          .on('click', function (ev) {
+            ev.preventDefault();
+            $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top - iniPos});
+          });
 
-    // handler for navbar stacked 
-    if ($(this).scrollTop() >= iniPos) {
-      navbar.addClass('stacked');
-    } else {
-      navbar.removeClass('stacked');
-    }
+      // when page is reloaded and it doesn't start on very top
+      // just attach the 'stacked' class to navbar to assure it will be
+      // available even on the middle of page.
+      if (iniPos > 0) {
+        navbar.addClass('stacked');
+      }
 
-  });
+    })
+
+    .on('scroll', function () {
+
+      // handler for navbar stacked
+      if ($(this).scrollTop() >= iniPos) {
+        navbar.addClass('stacked');
+      } else {
+        navbar.removeClass('stacked');
+      }
+
+    });
 
 }(jQuery));
