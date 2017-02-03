e2b1ed28cf46272d0e3efcf63a5c12c82626b12c
Rogerio Taques <rogerio.taques@rakuten.com>
2017-02-03 12:06:31 +0900

    Updates sample page

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,1 +1,142 @@
-!function(t){var s,e,o,a,i,h=20;t(document).on("ready",function(){s=t("#samples-nav"),e=s.offset().top,o=t("#license").offset().top-h,a=t(this).scrollTop()+h,i=s.width(),s.find("a.spy").each(function(){var s=t(t(this).attr("href"));t(this).data("offset",s.offset().top+s.height())}).on("click",function(s){t("html,body").animate({scrollTop:t(t(this).attr("href")).offset().top})}),a>=e&&0===t("#samples-nav.stacked").length&&(a+s.height()>=o&&(a-=a+s.height()-o),s.addClass("stacked").css({width:i,top:Math.fround(a-e)}))}).on("scroll resize",function(){a=t(this).scrollTop()+h,a>=e&&a+s.height()<=o?s.addClass("stacked").css({width:i,top:Math.fround(a-e)}):a<e&&s.removeClass("stacked").css({}),t(".spy").removeClass("active").each(function(){if(a<t(this).data("offset"))return t(this).addClass("active"),!1}),t.get("https://img.shields.io/github/release/rogeriotaques/seed-css.json",function(s){t("#release").html(s.value)})})}(jQuery);
+;(function ($) {
+
+  var navbar, iniPos, limitPos, curPos, navWidth, gutter = 20;
+
+  $(document)
+
+    .on('ready', function () {
+
+      navbar   = $('#samples-nav');
+      iniPos   = navbar.offset().top;
+      limitPos = $('#license').offset().top;
+      curPos   = $(this).scrollTop();
+      navWidth = navbar.width();
+
+      // handler for smooth scrolling
+      navbar
+        .find('a.spy')
+          .each(function () {
+            var target = $( $(this).attr('href') );
+            $(this).data('offset', target.offset().top + target.height());
+          })
+          .on('click', function (ev) {
+            $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top});
+          });
+
+      // when page is reloaded and it doesn't start on very top
+      // just attach the 'stacked' class to navbar to assure it will be
+      // available even on the middle of page.
+      if (curPos >= iniPos && $('#samples-nav.stacked').length === 0) {
+        if ( curPos + navbar.height() >= limitPos - 20 ) {
+          curPos -= (curPos + navbar.height() - limitPos + 20 );
+        }
+
+        navbar
+          .addClass('stacked')
+          .css({
+            width: navWidth,
+            top: Math.fround( curPos - iniPos ) + 20
+          });
+      }
+
+      // Set handler for users play with columns
+      $('#add-col')
+        .off('click')
+        .on('click', function (evt) {
+          if (evt) {
+            evt.preventDefault();
+          }
+
+          var row  = $('#flexible-cols');
+          var cols = row.find('.col');
+
+          if ( cols.length < 12 ) {
+            var count  = cols.length;
+            var remain = 12 - count;
+            var lastClass = 'sm-' + remain;
+
+            row.find('.col').removeClass('sm-1 sm-2 sm-3 sm-4 sm-5 sm-6 sm-7 sm-8 sm-9 sm-10 sm-11 sm-12');
+            row.find('.col:last').after( $('<div />', {'class': 'col align-center'}).html( count + 1 ) );
+            row.find('.col').each(function () {
+              $(this).addClass('sm-1');
+            });
+            row.find('.col:last').toggleClass('sm-1 ' + lastClass);
+
+            $('#rem-col').attr('disabled', false);
+          }
+          
+          if (cols.length >= 11) {
+            $(this).attr('disabled', true);
+          }
+        });
+
+      $('#rem-col')
+        .off('click')
+        .on('click', function (evt) {
+          if (evt) {
+            evt.preventDefault();
+          }
+
+          var row  = $('#flexible-cols');
+          var cols = row.find('.col');
+
+          if ( cols.length > 1 ) {
+            var remain = cols.length - 1;
+            var lastClass = 'sm-' + (12 - remain + 1);
+
+            row.find('.col:last').remove();
+            row.find('.col:last')
+              .removeClass('sm-1 sm-2 sm-3 sm-4 sm-5 sm-6 sm-7 sm-8 sm-9 sm-10 sm-11 sm-12')
+              .addClass(lastClass);
+
+            $('#add-col').attr('disabled', false);
+          } 
+          
+          if (cols.length <= 2) {
+            $(this).attr('disabled', true);
+          }
+        });
+
+    })
+
+    .on('scroll resize', function () {
+      curPos = $(this).scrollTop() + gutter;
+
+      // handler for navbar stacked
+      if (curPos >= iniPos && (curPos + navbar.height()) <= limitPos) {
+        navbar
+          .addClass('stacked')
+          .css({
+            width: navWidth,
+            top: Math.fround( curPos - iniPos )
+          });
+      } else if (curPos < iniPos)  {
+        navbar.removeClass('stacked').css({});
+      }
+
+      $('.spy')
+        .removeClass('active')
+        .each(function () {
+          if (curPos < $(this).data('offset')) {
+            $(this).addClass('active');
+            return false;
+          }
+        });
+
+      // get version shield from github/shilds.io
+      // $.get('https://img.shields.io/github/release/rogeriotaques/seed-css.json', function (data) {
+      //   $('#release').html( data.value );
+      // });
+
+    });
+
+  // Makes possible the blur on heaader while scrolling.
+  $(window).on('scroll', function() {
+    var scroll = $(this).scrollTop();
+
+    $("header").css({
+      'background-size': (100 + scroll/20) + "% " + (100 + scroll/20) + "%",
+    });
+  });
+
+}(jQuery));
