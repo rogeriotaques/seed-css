5db7ea75f68e43b6206bed749683ef8e103678d6
Rogerio Taques <rogerio@awin.com.br>
2017-03-29 23:07:48 +0900

    Done with a few more things, including vanilla js for input-file handler

--- source/js/main.js
+++ source/js/main.js
@@ -144,4 +144,44 @@
       // });
     });
 
+  
+    // Makes the sample custom input file works
+    var inputs = document.querySelectorAll('.seed-file > [type="file"]');
+    
+    inputs.forEach(function (input, i) {
+      var label = input.nextElementSibling;
+
+      input.addEventListener('change', function (evt) {
+        if (evt) {
+          evt.preventDefault();
+        }
+
+        var fileName = false;
+
+        if (this.files && this.files.length > 1) {
+          fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+        } else if (this.value.trim().length > 0) {
+          fileName = this.value.split('\\').pop();
+        }
+
+        if (fileName) {
+          label.querySelector('.input').innerHTML = fileName;
+        }
+
+        if (input.value !== '') {
+          input.parentNode.classList.add('chosen');
+        }
+      });
+
+      // label.querySelector('.cancel').addEventListener('click', function (evt) {
+      //   if (evt) {
+      //     evt.preventDefault();
+      //   }
+
+      //   input.value = '';
+      //   input.parent.classList.remove('chosen');
+      //   label.querySelector('.input').innerHTML = '';
+      // });
+    });
+
 }(jQuery));
