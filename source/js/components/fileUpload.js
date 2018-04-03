47e6560426d047b441ad80e790137c271bb0e6c8
Rogerio Taques <rogerio@awin.com.br>
2018-04-03 16:59:12 +0900

    Source code files version update

--- source/js/components/fileUpload.js
+++ source/js/components/fileUpload.js
@@ -2,7 +2,7 @@
  * Seed-CSS HTML Custom Input File.
  * @author Rogerio Taques (hello@abtz.co)
  * @see https://github.com/AbtzLabs/seed-css
- * @version 1.1.0
+ * @version 1.2.2
  * @license MIT
  */
 
