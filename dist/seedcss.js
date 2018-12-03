2e32e8c9de3d69e9f13b2aa467604fca17b820f0
Rogerio Taques <rogerio.taques@gmail.com>
2018-12-03 17:39:11 +0900

    Fixes minor issue with tooltips sample

--- dist/seedcss.js
+++ dist/seedcss.js
@@ -4,7 +4,7 @@
  * @author Rogerio Taques (hello@abtz.co) 
  * @copyright 2016-2018, Rogerio Taques (hello@abtz.co) 
  * @license MIT 
- * @version 2.0.1 
+ * @version 2.0.2 
  */ 
 
  "use strict";!function(){function t(t,e){e=e||{bubbles:!1,cancelable:!1,detail:void 0};var n=document.createEvent("CustomEvent");return n.initCustomEvent(t,e.bubbles,e.cancelable,e.detail),n}return"function"!=typeof window.CustomEvent&&(t.prototype=window.Event.prototype,void(window.CustomEvent=t))}();
